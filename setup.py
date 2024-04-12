312c1811f9f9dddcc092fe983cb7d15c33646f63
TVC Devs <tvc.deploy.bot@gmail.com>
2024-04-12 15:18:33 +0800

    Update psycopg2-binary

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setup(name='fsm',
       license='MIT',
       packages=find_packages(),
       test_suite='nose.collector',
-      install_requires=['colorlog==6.7.0', 'psycopg2-binary==2.9.6', 'sqlalchemy==2.0.9'],
+      install_requires=['colorlog==6.7.0', 'psycopg2-binary==2.9.9', 'sqlalchemy==2.0.9'],
       tests_require=['nose', 'pytest', 'mock', 'nosexcover', 'mypy', 'mongomock', 'mongoengine'],
       zip_safe=False)
